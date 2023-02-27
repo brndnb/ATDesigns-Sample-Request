d989e88fbc9a948578890dd3e483a527510e6913
Daniel Belleza <bellezabrndn@gmail.com>
2023-02-27 16:22:41 -0500

    initial set up

---
+++ items.js
@@ -0,0 +1,87 @@
+const items = [
+    {
+        "ItemId": "SAMPLE 100",
+        "Description": "PREPRODUCTION SAMPLE LAPEL PIN",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 100PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER  LAPEL",
+        "HS_Tariff": "7117119000"
+    },
+    {
+        "ItemId": "SAMPLE 650PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER BELT BUCKLE",
+        "HS_Tariff": "8308906000"
+    },
+    {
+        "ItemId": "SAMPLE 650Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC BELT BUCKLE",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 890PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER KEY TAG ",
+        "HS_Tariff": "7326200070"
+    },
+    {
+        "ItemId": "SAMPLE 890Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC KEY TAG",
+        "HS_Tariff": "7326200070"
+    },
+    {
+        "ItemId": "SAMPLE 909PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER DOG TAG",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 909Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC DOG TAG",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 910PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER MEDAL",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 910Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC MEDAL",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 911PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER PLAQUE",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 911Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC PLAQUE ME",
+        "HS_Tariff": "7117199000"
+    },
+    {
+        "ItemId": "SAMPLE 920PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER KEY TO CITY",
+        "HS_Tariff": "8306290000"
+    },
+    {
+        "ItemId": "SAMPLE 940PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER BAG TAG",
+        "HS_Tariff": "8007005000"
+    },
+    {
+        "ItemId": "SAMPLE 940Z",
+        "Description": "PREPRODUCTION SAMPLE ZINC BAG TAG",
+        "HS_Tariff": "8306290000"
+    },
+    {
+        "ItemId": "SAMPLE 950PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER XMAS ORNAMENT",
+        "HS_Tariff": "9505102500"
+    },
+    {
+        "ItemId": "SAMPLE PW",
+        "Description": "PREPRODUCTION SAMPLE PEWTER GIFT AC",
+        "HS_Tariff": "9505102500"
+    }
+]
